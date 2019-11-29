5f0c9ac1a65c6f88c5c7b42affe12b7c88e3804d
Владислав Пономарёв <pvbkis555@gmail.com>
2019-11-29 21:20:11 +0300

    Update AnalyzePage #2

--- FAVAC/FAVAC/AnalyzePage.xaml.cs
+++ FAVAC/FAVAC/AnalyzePage.xaml.cs
@@ -39,11 +39,9 @@ namespace FAVAC
                 if ((s as ToolbarItem).Text == "PROFILE")
                 {
                     (s as ToolbarItem).Text = "MAIN";
-                    Title = "PROFILE";
                 } else
                 {
                     (s as ToolbarItem).Text = "PROFILE";
-                    Title = "MAIN";
                 }
                 webCardsLoad();
             };
@@ -58,20 +56,20 @@ namespace FAVAC
                 HorizontalOptions = LayoutOptions.FillAndExpand,
                 VerticalOptions = LayoutOptions.StartAndExpand,
                 BackgroundColor = Color.Black,
-                Margin = new Thickness(-2,0,-1,0)
+                Margin = new Thickness(-2, 0, -1, 0)
             };
             switch (id)
             {
                 case 0:
-                    HtmlWebViewSource htmlWebViewSource = new HtmlWebViewSource();
-                    htmlWebViewSource.Html = @"<html><body"  + " style=\"padding: 0; margin: 0; background-color: #1E222D;\"" + ">" +
-                        "<div class=\"tradingview-widget-container\">" + 
+                    HtmlWebViewSource htmlWebViewSource0 = new HtmlWebViewSource();
+                    htmlWebViewSource0.Html = @"<html><body" + " style=\"padding: 0; margin: 0; background-color: #1E222D;\"" + ">" +
+                        "<div class=\"tradingview-widget-container\">" +
                         "<script type=\"text/javascript\" src=\"https://s3.tradingview.com/external-embedding/embed-widget-symbol-info.js\" async>{" +
                         $"\"symbol\": \"{symbol}\",\"width\": \"100%\",\"locale\": \"{Settings.Language}\",\"colorTheme\": \"dark\",\"isTransparent\": false" +
                         "}</script>" +
                         "</div></body></html>";
-                    webView.HeightRequest = (DeviceDisplay.MainDisplayInfo.Orientation == DisplayOrientation.Portrait) ? 240 : 123;
-                    webView.Source = htmlWebViewSource;
+                    webView.HeightRequest = (DeviceDisplay.MainDisplayInfo.Orientation == DisplayOrientation.Portrait) ? 241 : 123;
+                    webView.Source = htmlWebViewSource0;
                     // https://s.tradingview.com/embed-widget/symbol-info/?locale=en&amp;symbol=EURRUB#%7B%22symbol%22%3A%22EURRUB%22%2C%22width%22%3A%22100%25%22%2C%22colorTheme%22%3A%22dark%22%2C%22isTransparent%22%3Afalse%2C%22height%22%3A206%2C%22utm_source%22%3A%22%22%2C%22utm_medium%22%3A%22widget_new%22%2C%22utm_campaign%22%3A%22symbol-info%22%7D
                     // https://s.tradingview.com/embed-widget/mini-symbol-overview/?locale=ru#%7B%22symbol%22%3A%22AAAP%22%2C%22width%22%3A%22100%25%22%2C%22height%22%3A%22100%25%22%2C%22dateRange%22%3A%2212m%22%2C%22colorTheme%22%3A%22dark%22%2C%22trendLineColor%22%3A%22%2337a6ef%22%2C%22underLineColor%22%3A%22rgba(55%2C%20166%2C%20239%2C%200.15)%22%2C%22isTransparent%22%3Afalse%2C%22autosize%22%3Atrue%2C%22largeChartUrl%22%3A%22%22%2C%22utm_source%22%3A%22%22%2C%22utm_medium%22%3A%22widget_new%22%2C%22utm_campaign%22%3A%22mini-symbol-overview%22%7D
                     break;
@@ -83,13 +81,31 @@ namespace FAVAC
                     // $"https://s.tradingview.com/embed-widget/technical-analysis/?locale=en#%7B%22interval%22%3A%225m%22%2C%22width%22%3A%22100%25%22%2C%22colorTheme%22%3A%22dark%22%2C%22isTransparent%22%3Afalse%2C%22height%22%3A%22100%25%22%2C%22symbol%22%3A%22{symbol}%22%2C%22showIntervalTabs%22%3Atrue%2C%22utm_source%22%3A%22%22%2C%22utm_medium%22%3A%22widget_new%22%2C%22utm_campaign%22%3A%22technical-analysis%22%7D";
                     break;
                 case 2:
+                    //string url = "https://www.investing.com/news/";
+                    //HtmlWeb webDoc = new HtmlWeb();
+                    //HtmlDocument doc = webDoc.Load(url);
+                    //HtmlNodeCollection par2 = doc.DocumentNode.SelectNodes("//div[@class=\"textDiv\"]//p");
+
+                    //foreach (var tag in par2)
+                    //{
+                    //    newsCards.Children.Add(newsCard(tag.InnerText));
+                    //}
+
+                    break;
+                case 3:
+                    uriBuilder.Path = "/embed-widget/mini-symbol-overview/";
+                    uriBuilder.Query = $"?locale={Settings.Language}#%7B%22symbol%22%3A%22{symbol}" + "%22%2C%22width%22%3A%22100%25%22%2C%22height%22%3A%22100%25%22%2C%22dateRange%22%3A%2212m%22%2C%22colorTheme%22%3A%22dark%22%2C%22trendLineColor%22%3A%22rgba(0%2C%20255%2C%20255%2C%201)%22%2C%22underLineColor%22%3A%22rgba(0%2C%20255%2C%20255%2C%200.15)%22%2C%22isTransparent%22%3Afalse%2C%22autosize%22%3Atrue%2C%22largeChartUrl%22%3A%22%22%2C%22utm_source%22%3A%22%22%2C%22utm_medium%22%3A%22widget_new%22%2C%22utm_campaign%22%3A%22mini-symbol-overview%22%7D";
+                    webView.HeightRequest = 300;
+                    webView.Source = uriBuilder.Uri;
+                    break;
+                case 4:
                     uriBuilder.Path = "/embed-widget/financials/";
                     uriBuilder.Query = $"?locale={Settings.Language}#%7B%22symbol%22%3A%22{symbol}%22%2C%22colorTheme%22%3A%22dark%22%2C%22isTransparent%22%3Afalse%2C%22largeChartUrl%22%3A%22%22%2C%22displayMode%22%3A%22adaptive%22%2C%22width%22%3A%22100%25%22%2C%22height%22%3A%22100%25%22%2C%22utm_source%22%3A%22%22%2C%22utm_medium%22%3A%22widget_new%22%2C%22utm_campaign%22%3A%22financials%22%7D";
                     webView.HeightRequest = 489;
                     webView.Source = uriBuilder.Uri;
                     // $"https://s.tradingview.com/embed-widget/financials/?locale=en#%7B%22symbol%22%3A%22{symbol}%22%2C%22colorTheme%22%3A%22dark%22%2C%22isTransparent%22%3Afalse%2C%22largeChartUrl%22%3A%22%22%2C%22displayMode%22%3A%22adaptive%22%2C%22width%22%3A%22100%25%22%2C%22height%22%3A%22100%25%22%2C%22utm_source%22%3A%22%22%2C%22utm_medium%22%3A%22widget_new%22%2C%22utm_campaign%22%3A%22financials%22%7D";
                     break;
-                case 3:
+                case 5:
                     uriBuilder.Path = "/embed-widget/symbol-profile/";
                     uriBuilder.Query = $"?locale={Settings.Language}&amp;symbol={symbol}#%7B%22width%22%3A%22100%25%22%2C%22height%22%3A%22100%25%22%2C%22colorTheme%22%3A%22dark%22%2C%22isTransparent%22%3Afalse%2C%22utm_source%22%3A%22%22%2C%22utm_medium%22%3A%22widget_new%22%2C%22utm_campaign%22%3A%22symbol-profile%22%7D";
                     webView.HeightRequest = 800;
@@ -97,13 +113,15 @@ namespace FAVAC
                     // $"https://s.tradingview.com/embed-widget/symbol-profile/?locale=en&amp;symbol={symbol}#%7B%22width%22%3A%22100%25%22%2C%22height%22%3A%22100%25%22%2C%22colorTheme%22%3A%22dark%22%2C%22isTransparent%22%3Afalse%2C%22utm_source%22%3A%22%22%2C%22utm_medium%22%3A%22widget_new%22%2C%22utm_campaign%22%3A%22symbol-profile%22%7D";
                     break;
             }
-          //  webView.Navigating += (s, e) => { e.Cancel = (ready) ? true : false; if (!ready) ready = true; };
+            webView.Navigating += (s, e) => { e.Cancel = (ready) ? true : false; };
+            webView.Navigated += (s, e) => { ready = true; };
 
             return webView;
         }
 
         void webCardsLoad()
         {
+            ready = false;
             webCards.Children.Clear();
             if (toolbarItem_switchView.Text == "PROFILE")
             {
@@ -111,8 +129,9 @@ namespace FAVAC
                 webCards.Children.Add(_webView(1));
             } else
             {
-                webCards.Children.Add(_webView(2));
                 webCards.Children.Add(_webView(3));
+                webCards.Children.Add(_webView(4));
+                webCards.Children.Add(_webView(5));
             }
         }
 
