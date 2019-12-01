25a40dc8bf7ebb87f49c51a70e4be276b9fa36a6
Владислав Пономарёв <pvbkis555@gmail.com>
2019-12-01 22:36:52 +0300

    Update MorePage

--- FAVAC/FAVAC/MorePage.xaml.cs
+++ FAVAC/FAVAC/MorePage.xaml.cs
@@ -51,7 +51,7 @@ namespace FAVAC
             new CommonMenuItem {Title="Forex Heat Map", ImagePath="ic_chart_scatter_plot.png" },
             new CommonMenuItem {Title="Screener", ImagePath="ic_chevron_double_up.pngg" },
             new CommonMenuItem {Title="Cryptocurrency Market", ImagePath="ic_bitcoin.png" }
-        };
+            };
 
             ElementsOthers = new List<CommonMenuItem>
             {
@@ -67,13 +67,39 @@ namespace FAVAC
             public string ImagePath { get; set; }
         }
         
-        private void itemsList_ItemTapped(object sender, ItemTappedEventArgs e)
+        private async void itemsList_ItemTapped(object sender, ItemTappedEventArgs e)
         {
             if (e.Item != null)
+            {
                 ((ListView)sender).SelectedItem = null;
+                await Navigation.PushAsync(new WebViewHand());
+                switch (e.ItemIndex)
+                {
+                    case 0:
+                        MessagingCenter.Send((e.Item as CommonMenuItem).Title + "|https://s.tradingview.com/embed-widget/hotlists/?locale=en#%7B%22colorTheme%22%3A%22dark%22%2C%22dateRange%22%3A%2212m%22%2C%22exchange%22%3A%22US%22%2C%22showChart%22%3Atrue%2C%22width%22%3A%22100%25%22%2C%22height%22%3A%22100%25%22%2C%22largeChartUrl%22%3A%22%22%2C%22isTransparent%22%3Afalse%2C%22plotLineColorGrowing%22%3A%22rgba(25%2C%20118%2C%20210%2C%201)%22%2C%22plotLineColorFalling%22%3A%22rgba(25%2C%20118%2C%20210%2C%201)%22%2C%22gridLineColor%22%3A%22rgba(42%2C%2046%2C%2057%2C%201)%22%2C%22scaleFontColor%22%3A%22rgba(120%2C%20123%2C%20134%2C%201)%22%2C%22belowLineFillColorGrowing%22%3A%22rgba(33%2C%20150%2C%20243%2C%200.12)%22%2C%22belowLineFillColorFalling%22%3A%22rgba(33%2C%20150%2C%20243%2C%200.12)%22%2C%22symbolActiveColor%22%3A%22rgba(33%2C%20150%2C%20243%2C%200.12)%22%2C%22utm_source%22%3A%22www.tradingview.com%22%2C%22utm_medium%22%3A%22widget_new%22%2C%22utm_campaign%22%3A%22hotlists%22%7D", "SetWebViewKey");
+                        break;
+                    case 1:
+                        MessagingCenter.Send((e.Item as CommonMenuItem).Title + "|https://s.tradingview.com/embed-widget/forex-cross-rates/?locale=en#%7B%22width%22%3A%22100%25%22%2C%22height%22%3A%22100%25%22%2C%22currencies%22%3A%5B%22EUR%22%2C%22USD%22%2C%22JPY%22%2C%22GBP%22%2C%22CHF%22%2C%22AUD%22%2C%22CAD%22%2C%22NZD%22%2C%22CNY%22%5D%2C%22utm_source%22%3A%22%22%2C%22utm_medium%22%3A%22widget_new%22%2C%22utm_campaign%22%3A%22forex-cross-rates%22%7D", "SetWebViewKey");
+                        break;
+                    case 2:
+                        MessagingCenter.Send((e.Item as CommonMenuItem).Title + "|https://s.tradingview.com/embed-widget/forex-heat-map/?locale=en#%7B%22width%22%3A%22100%25%22%2C%22height%22%3A%22100%25%22%2C%22currencies%22%3A%5B%22EUR%22%2C%22USD%22%2C%22JPY%22%2C%22GBP%22%2C%22CHF%22%2C%22AUD%22%2C%22CAD%22%2C%22NZD%22%2C%22CNY%22%5D%2C%22utm_source%22%3A%22%22%2C%22utm_medium%22%3A%22widget_new%22%2C%22utm_campaign%22%3A%22forex-heat-map%22%7D", "SetWebViewKey");
+                        break;
+                    case 3:
+                        MessagingCenter.Send((e.Item as CommonMenuItem).Title + "|https://s.tradingview.com/embed-widget/screener/?locale=en#%7B%22width%22%3A%22100%25%22%2C%22height%22%3A%22100%25%22%2C%22defaultColumn%22%3A%22overview%22%2C%22defaultScreen%22%3A%22general%22%2C%22market%22%3A%22forex%22%2C%22showToolbar%22%3Atrue%2C%22colorTheme%22%3A%22dark%22%2C%22enableScrolling%22%3Atrue%2C%22utm_source%22%3A%22%22%2C%22utm_medium%22%3A%22widget_new%22%2C%22utm_campaign%22%3A%22forexscreener%22%7D", "SetWebViewKey");
+                        break;
+                    case 4:
+                        MessagingCenter.Send((e.Item as CommonMenuItem).Title + "|https://s.tradingview.com/embed-widget/crypto-mkt-screener/?locale=en#%7B%22width%22%3A%22100%25%22%2C%22height%22%3A%22100%25%22%2C%22defaultColumn%22%3A%22overview%22%2C%22screener_type%22%3A%22crypto_mkt%22%2C%22displayCurrency%22%3A%22USD%22%2C%22colorTheme%22%3A%22dark%22%2C%22market%22%3A%22crypto%22%2C%22enableScrolling%22%3Atrue%2C%22utm_source%22%3A%22%22%2C%22utm_medium%22%3A%22widget_new%22%2C%22utm_campaign%22%3A%22cryptomktscreener%22%7D", "SetWebViewKey");
+                        break;
+                }
+            }
         }
 
         private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+
+        }
+
+        private void itemsListOther_ItemTapped(object sender, ItemTappedEventArgs e)
         {
 
         }
---
+++ FAVAC/FAVAC/WebViewHand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace FAVAC
+{
+    public class WebViewHand : ContentPage
+    {
+        public WebViewHand()
+        {
+            WebView webView = new WebView
+            {
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                Margin = new Thickness(-2)
+            };
+            MessagingCenter.Subscribe<string>(this, "SetWebViewKey", _source => Device.BeginInvokeOnMainThread(() =>
+            {
+                string[] data = _source.Split('|');
+                Title = data[0];
+                webView.Source = data[1];
+            }));
+            Content = webView;
+        }
+    }
+}
