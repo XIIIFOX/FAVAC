a84bd9827a8989b5ee8f6ec8f262e396cb80f11a
Владислав Пономарёв <pvbkis555@gmail.com>
2019-12-13 19:36:53 +0300

    PreRelease UWP and some fix

---
+++ FAVAC/FAVAC.Android/obj/Release/100/android/typemap.jm.x86.s
@@ -0,0 +1,25 @@
+	/* Data Hash: C57E162245AD7DF2 */
+	.file	"typemap.jm.inc"
+
+	/* Mapping header */
+	.section	.data.jm_typemap,"aw",@progbits
+	.type	jm_typemap_header, @object
+	.p2align	2
+	.global	jm_typemap_header
+jm_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	869
+	/* entry-length */
+	.long	232
+	/* value-offset */
+	.long	102
+	.size	jm_typemap_header, 16
+
+	/* Mapping data */
+	.type	jm_typemap, @object
+	.global	jm_typemap
+jm_typemap:
+	.size	jm_typemap, 201609
+	.include	"typemap.jm.inc"
---
+++ FAVAC/FAVAC.Android/obj/Release/100/android/typemap.mj.x86_64.s
@@ -0,0 +1,25 @@
+	/* Data Hash: 55481E1EA121B5AE */
+	.file	"typemap.mj.inc"
+
+	/* Mapping header */
+	.section	.data.mj_typemap,"aw",@progbits
+	.type	mj_typemap_header, @object
+	.p2align	2
+	.global	mj_typemap_header
+mj_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	997
+	/* entry-length */
+	.long	232
+	/* value-offset */
+	.long	130
+	.size	mj_typemap_header, 16
+
+	/* Mapping data */
+	.type	mj_typemap, @object
+	.global	mj_typemap
+mj_typemap:
+	.size	mj_typemap, 231305
+	.include	"typemap.mj.inc"
